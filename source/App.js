b3b4104618cb62094af687d000553a5ec16cae1a
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2012-10-05 15:38:41 -0700

    adding JobManager logs to App.js

--- source/App.js
+++ source/App.js
@@ -6,14 +6,15 @@ enyo.kind({
 		{kind: "JobManager", name: "jobManager", onNewJobList: "updateJobTable"},
 		{kind: "QueryBuilder", style: "height: 30%", onQueryChanged: "getJobList"},
 		{kind: "Panels", components: [
-			{kind: "JobTable"},
+			//{kind: "JobTable"},
 			//{kind: "JobView"},
 		]},
 	],
 	getJobList: function(inSender, inEvent) {
-		console.log(arguments);
-	}
-	updateJobTable: function(inSender, inEvent) {
+		console.log("getJobList", arguments);
 		this.$.jobManager.getJobList(inEvent);
+	},
+	updateJobTable: function(inSender, inEvent) {
+		console.log("updateJobTable", inSender, inEvent);
 	},
 });
