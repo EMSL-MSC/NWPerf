7b13269a76ae90840f264b68173158e6c327b32b
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2012-10-11 16:33:53 -0700

    Added Job View panel

---
+++ source/JobView.js
@@ -0,0 +1,58 @@
+enyo.kind({
+	name: "JobView",
+	kind: "FittableRows",
+	classes: "dark",
+	published: {
+		job: false,
+	},
+	events: {
+		onJobViewClosed: "",
+	},
+	components:[
+		{kind: "Scroller", fit: true, components: [
+			{name: "jobGraphs"},
+		]},
+		{kind: "onyx.Toolbar", components: [
+			{kind: "onyx.Button", ontap: "doJobViewClosed", content: "Close"},
+		]},
+	],
+	spin: function() {
+		this.$.jobGraphs.destroyClientControls();
+		this.$.jobGraphs.createComponent({kind: "onyx.Spinner"});
+		this.$.jobGraphs.render();
+	},
+	jobChanged: function(oldValue) {
+		this.$.jobGraphs.destroyClientControls();
+		this.$.jobGraphs.render();
+		length = 0;
+		for(group in this.job) {
+			if(group == "originator") {
+				continue;
+			}
+			length++;
+			groupHeader = this.$.jobGraphs.createComponent({content: group, ontap: "toggleDrawer", classes: "group-header"}, {owner:  this});
+			groupDrawer = this.$.jobGraphs.createComponent({kind: "onyx.Drawer", open: false});
+			groupHeader.drawer = groupDrawer;
+			for(metric in this.job[group]) {
+				headerText = this.job[group][metric]["name"]+" - "+ this.job[group][metric]["description"];
+				metricHeader = groupDrawer.createComponent({content: headerText, ontap: "toggleDrawer", classes: "image-header"}, {owner:  this})
+				metricDrawer = groupDrawer.createComponent({kind: "onyx.Drawer", open: false});
+				metricHeader.drawer = metricDrawer;
+				metricDrawer.createComponent({kind: "Image", src: this.job[group][metric]["src"], ontap: "toggleThumbnail", classes: "thumbnail", thumbnail: true}, {owner: this});
+			}
+		}
+		console.log("Length", length);
+		if(length == 0) {
+			this.$.jobGraphs.createComponent({content: "Sorry there is no data for this job", classes:"job-error"});
+		}
+		this.$.jobGraphs.render();
+	},
+	toggleDrawer: function(inSender, inEvent) {
+		console.log("Group tapped", inSender, inEvent);
+		inSender.drawer.setOpen(! inSender.drawer.getOpen());
+	},
+	toggleThumbnail: function(inSender, inEvent) {
+		inSender.thumbnail = !inSender.thumbnail;
+		inSender.addRemoveClass("thumbnail", inSender.thumbnail);
+	},
+});
