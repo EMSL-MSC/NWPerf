8b7d94813cec89dbc8e381e3e2e5c0279fa16a3f
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2012-10-22 21:30:51 -0700

    Adding new version plots and legends

---
+++ source/Flot.js
@@ -0,0 +1,141 @@
+enyo.kind({
+	name:"Flot",
+	kind:"Control",
+	published:{
+       		src: null,
+       		legend: null,
+	},
+	events: {
+		onValuesUpdate: "",
+	},
+	graphData: null,
+	hasPlotted: false,
+	legendChanged: function(oldValue) {
+		if(this.hasPlotted) {
+			this.plot()
+		}
+	},
+	updateValuesTimeout: null,
+	latestPosition: null,
+	previousPoint: null,
+ 	scheduleUpdateValues: function (event, pos, item) {
+		owner = event.data.owner;
+		owner.latestPosition = pos;
+
+		if (item) {
+			if(this.previousPoint != item.dataIndex) {
+				previousPoint = item.dataIndex;
+				$("#tooltip").remove();
+				y = item.datapoint[1].toPrecision(4);
+				owner.showTooltip(item.pageX, item.pageY, item.series.label + ": " + y);
+			}
+		} else {
+			$("#tooltip").remove();
+			this.previousPoint = null
+		}
+		if (!owner.updateValuesTimeout) {
+			owner.updateValuesTimeout = setTimeout(function(){
+
+				owner.updateValuesTimeout = null;
+
+				var pos = owner.latestPosition;
+
+				var axes = plot.getAxes();
+				if (pos.x < axes.xaxis.min && pos.x > axes.xaxis.max)
+					return;
+
+				var i, j, dataset = plot.getData();
+				values = {};
+				for (i = 0; i < dataset.length; ++i) {
+					var series = dataset[i];
+
+					// find the nearest points, x-wise
+					for (j = 0; j < series.data.length; ++j)
+						if (series.data[j][0] > pos.x)
+							break;
+					// now interpolate
+					var y, p1 = series.data[j - 1], p2 = series.data[j];
+					if (p1 == null)
+						y = p2[1];
+					else if (p2 == null)
+						y = p1[1];
+					else
+						y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+					//legends.eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2)));
+					values[series.label] = y.toPrecision(4);
+				}
+				owner.doValuesUpdate(values);
+			}, 50);
+		}
+	},
+	plot: function() {
+		var n = this.hasNode();
+		if(n) {
+			if(this.graphData) {
+				this.hasPlotted = true;
+				data = []
+				for(host in this.graphData["data"]) {
+					points = [];
+					color = "#000000";
+					for(entry in this.legend) {
+						entry = this.legend[entry];
+						if(entry.host == host && entry.enabled) {
+							color = "hsl("+entry.color[0]+", "+(entry.color[1]*100)+"%, "+(entry.color[2]*100)+"%)";
+							data.push({lines: { show: true }, color: color, label: host, data: this.graphData["data"][host]});
+							break;
+						}
+					}
+				}
+				plot = $.plot(jQuery(n), data, {
+					grid: {
+						hoverable: true,
+					},
+					crosshair: { mode: "x"},
+					legend: {
+						show: false,
+					},
+					"yaxis": {
+						tickFormatter: this.siTicks,
+						//labelWidth: 30
+					},
+					"xaxis": {
+						mode: "time",
+						//min: this.graphData["startTime"],
+						//max: this.graphData["endTime"],
+						//labelHeight: 20
+					}
+				});
+				jQuery(n).bind("plothover",{owner: this}, this.scheduleUpdateValues);
+			} else {
+				new enyo.Ajax({url: this.src})
+				.response(this, function(inSender, inResponse) {
+					this.graphData = inResponse;
+					this.plot();
+				})
+				.go();
+			}
+		}
+	},
+	siTicks: function(val, axis) {
+		units = ["", "K", "M", "G", "T", "P"];
+		for(unit in units) {
+			if(val < 1000) {
+				return val+units[unit];
+			} else {
+				val /= 1000.0;
+			}
+		}
+	},
+	showTooltip: function(x, y, contents) {
+		$('<div id="tooltip">' + contents + '</div>').css( {
+			position: 'absolute',
+			display: 'none',
+			top: y - 20,
+			left: x + 5,
+			border: '1px solid #fdd',
+			padding: '2px',
+			'background-color': '#fee',
+			opacity: 0.80
+		}).appendTo("body").fadeIn(200);
+	}
+});
--- source/JobView.js
+++ source/JobView.js
@@ -9,13 +9,73 @@ enyo.kind({
 		onJobViewClosed: "",
 	},
 	components:[
-		{kind: "Scroller", fit: true, components: [
-			{name: "jobGraphs"},
+		{kind: "FittableColumns", fit: true, components: [
+			{kind: "Scroller", fit: true, components: [
+				{name: "jobGraphs"},
+			]},
+			{kind: "Scroller", classes: "legend", name: "legendScroller", components: [
+				{kind: "FittableColumns", components: [
+					{components: [
+						{kind: "Checkbox", onActivate: "toggleCheckboxes", active: true},
+					]},
+					{content: "Select All"},
+				]},
+				{kind: "Repeater", name: "legend", onSetupItem: "legendItem", components: [
+					{kind: "FittableColumns", components: [
+						{components: [
+							{name: "enable", kind: "Checkbox", onActivate: "legendChecked", host: ""},
+						]},
+						{classes: "color-border", components: [
+							{name: "color"},
+						]},
+						{name: "label", classes: "legend-label"},
+						{name: "value", classes: "legend-label"},
+					]},
+				]},
+			]},
 		]},
 		{kind: "onyx.Toolbar", components: [
 			{kind: "onyx.Button", ontap: "doJobViewClosed", content: "Close"},
 		]},
 	],
+	toggleCheckboxes: function(inSender, inEvent) {
+		for(i=0;i<this.legend.length;i++) {
+			this.legend[i].enabled = inSender.checked;
+		}
+		for(i=0;i<this.graphs.length;i++) {
+			this.graphs[i].setLegend(this.legend.slice(0));
+		}
+		this.$.legend.setCount(this.legend.length);
+	},
+	legendChecked: function(inSender, inEvent) {
+		for(i=0;i<this.legend.length;i++) {
+			if(this.legend[i].host == inSender.host) {
+				this.legend[i].enabled = inSender.checked;
+				break;
+			}
+		}
+		this.values[inSender.host].setContent("");
+		this.updateLegendValues()
+		for(i=0;i<this.graphs.length;i++) {
+			this.graphs[i].setLegend(this.legend.slice(0));
+		}
+	},
+	values: {},
+	legendItem: function(inSender, inEvent) {
+		item = this.legend[inEvent.index];
+		inEvent.item.$.enable.checked = item.enabled;
+		inEvent.item.$.enable.host = item.host
+		inEvent.item.$.color.setStyle("border: 5px solid hsl("+item.color[0]+", "+(item.color[1]*100)+"%, "+(item.color[2]*100)+"%);");
+		inEvent.item.$.label.setContent(item.host);
+		this.values[item.host] = inEvent.item.$.value;
+		return true;
+	},
+	updateLegendValues: function(inSender, inEvent) {
+		for(host in inEvent) {
+			if(host != "originator")
+				this.values[host].setContent(": " + inEvent[host]);
+		}
+	},
 	spin: function() {
 		this.$.jobGraphs.destroyClientControls();
 		this.$.jobGraphs.createComponent({style:"background:black; border-radius:5px; padding:15px; width:65px;", components: [
@@ -23,34 +83,67 @@ enyo.kind({
 		]});
 		this.$.jobGraphs.render();
 	},
+	legend: [],
+	graphs: [],
 	jobChanged: function(oldValue) {
-		console.log("jobChanged", this.job);
 		this.$.jobGraphs.destroyClientControls();
+		this.graphs = [];
+		this.values = {};
+		this.$.legendScroller.setShowing(false);
 		this.$.jobGraphs.render();
-		if(this.job["version"] = 1) {
-			length = 0;
-			for(group in this.job["graphs"]) {
-				console.log(group);
-				length++;
+		length = 0;
+		if(this.job["version"] == 2) {
+			this.legend = [];
+			numHosts = this.job["hosts"].length;
+			hue = hueFractions = 350/numHosts;
+			lightnessFractions = .3/numHosts;
+			lightness = .3;
+			for(host in this.job["hosts"]) {
+				hue += hueFractions;
+				lightness += lightnessFractions;
+				this.legend[host] = {host: this.job["hosts"][host], color: [hue, 1, lightness], enabled: true};
+			}
+			this.$.legend.setCount(numHosts);
+		}
+		for(group in this.job["graphs"]) {
+			length++;
+			if(group == "") {
+				groupHeader = this.$.jobGraphs.createComponent({content: "Other", ontap: "toggleDrawer", classes: "group-header"}, {owner:  this});
+			} else {
 				groupHeader = this.$.jobGraphs.createComponent({content: group, ontap: "toggleDrawer", classes: "group-header"}, {owner:  this});
-				groupDrawer = this.$.jobGraphs.createComponent({kind: "onyx.Drawer", open: false});
-				groupHeader.drawer = groupDrawer;
-				for(metric in this.job["graphs"][group]) {
-					headerText = this.job["graphs"][group][metric]["name"]+" - "+ this.job["graphs"][group][metric]["description"];
-					metricHeader = groupDrawer.createComponent({content: headerText, ontap: "toggleDrawer", classes: "image-header"}, {owner:  this})
-					metricDrawer = groupDrawer.createComponent({kind: "onyx.Drawer", open: false});
-					metricHeader.drawer = metricDrawer;
+			}
+			groupDrawer = this.$.jobGraphs.createComponent({kind: "onyx.Drawer", open: false});
+			groupHeader.drawer = groupDrawer;
+			for(metric in this.job["graphs"][group]) {
+				headerText = this.job["graphs"][group][metric]["name"]+" - "+ this.job["graphs"][group][metric]["description"];
+				metricHeader = groupDrawer.createComponent({content: headerText, ontap: "toggleDrawer", classes: "image-header"}, {owner:  this})
+				metricDrawer = groupDrawer.createComponent({kind: "onyx.Drawer", open: false});
+				metricHeader.drawer = metricDrawer;
+				if(this.job["version"] == 1) {
 					metricDrawer.createComponent({kind: "Image", src: this.job["graphs"][group][metric]["src"], ontap: "toggleThumbnail", classes: "thumbnail", thumbnail: true}, {owner: this});
+				} else {
+					this.graphs.push( metricDrawer.createComponent({kind: "Flot", legend: this.legend, src: this.job["graphs"][group][metric]["src"], classes: "graph", onValuesUpdate: "updateLegendValues"}, {owner: this}));
+					this.$.legendScroller.setShowing(true);
 				}
 			}
-			if(length == 0) {
-				this.$.jobGraphs.createComponent({content: "Sorry there is no data for this job", classes:"job-error"});
-			}
+		}
+		if(length == 0) {
+			this.$.jobGraphs.createComponent({content: "Sorry there is no data for this job", classes:"job-error"});
 		}
 		this.$.jobGraphs.render();
 	},
 	toggleDrawer: function(inSender, inEvent) {
-		inSender.drawer.setOpen(! inSender.drawer.getOpen());
+		if(inSender.drawer.getOpen()) {
+			inSender.drawer.setOpen(0);
+		} else {
+			components = inSender.drawer.getControls();
+			for(comp in components) {
+				if(components[comp].kind == "Flot") {
+					components[comp].plot();
+				}
+			}
+			inSender.drawer.setOpen(1);
+		}
 	},
 	toggleThumbnail: function(inSender, inEvent) {
 		inSender.thumbnail = !inSender.thumbnail;
