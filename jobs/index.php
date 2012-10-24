3853ef7e9eec62d4a731f5a500eecced72daae01
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2012-10-23 17:09:03 -0700

    Making the server find and extract new style graphs

--- jobs/index.php
+++ jobs/index.php
@@ -25,8 +25,8 @@
 				"Oct" => 10,
 				"Nov" => 11,
 				"Dec" => 12);
-		$fields = array("Start Date" => "start_time", "End Date" => "end_time", "Submit Date" => "submit_time", "Dispatch Date" => "dispatch_time", "Node Count" => "num_nodes_allocated", "User" => "moab_job_details.user", "Job Id" => "id", "Account" => "account");
-		$sql = "select id, moab_job_details.user as user, jobs_id as jobId, account, num_nodes_allocated as numNodes, submit_time as submitTime, start_time as startTime, end_time as endTime, end_time-start_time as runTime from moab_job_details where ";
+		$fields = array("Start Date" => "start_time", "End Date" => "end_time", "Submit Date" => "submit_time", "Dispatch Date" => "dispatch_time", "Node Count" => "num_nodes_allocated", "User" => "moab_job_details.user", "Job Id" => "jobs.job_id", "Account" => "account");
+		$sql = "select jobs.job_id as id, moab_job_details.user as user, jobs_id as jobId, account, num_nodes_allocated as numNodes, submit_time as submitTime, start_time as startTime, end_time as endTime, end_time-start_time as runTime from jobs, moab_job_details where jobs.id = moab_job_details.jobs_id and ";
 		for($i=0;$i<count($query);$i++) {
 			if($i != 0) {
 				$sql .= "and ";
@@ -72,26 +72,54 @@
 		print(json_encode($ret));
 	} else {
 		$jobs_id = $_GET["job"];
-		$query = $db->prepare("select point_description from point_descriptions where id = ?");
-		$pointsDir = "/var/www/nwperf-graphs/".($jobs_id%100)."/".($jobs_id/100%100)."/".$jobs_id;
-		$pointsDescFile = $pointsDir."/pointsDescriptions";
-		$file = fopen($pointsDescFile,'r');
-		$fstat = fstat($file);
-		$jobData = unserialize(fread($file, $fstat["size"]));
-		$ret = array();
-		foreach($jobData as $graph) {
-			if(file_exists($pointsDir."/job.$jobs_id-point.".$graph["point_id"].".png")) {
-				if(! array_key_exists($graph["group"], $ret)) {
-					$ret[$graph["group"]] = array();
+		$pointsArchive = "/var/www/nwperf-graphs/flot-graphs/".($jobs_id%100)."/".($jobs_id/100%100)."/$jobs_id.tar.bz2";
+		if(! file_exists($pointsArchive)) {
+			$query = $db->prepare("select point_description from point_descriptions where id = ?");
+			$pointsDir = "/var/www/nwperf-graphs/".($jobs_id%100)."/".($jobs_id/100%100)."/".$jobs_id;
+			$pointsDescFile = $pointsDir."/pointsDescriptions";
+			$file = fopen($pointsDescFile,'r');
+			$fstat = fstat($file);
+			$jobData = unserialize(fread($file, $fstat["size"]));
+			$ret = array();
+			foreach($jobData as $graph) {
+				if(file_exists($pointsDir."/job.$jobs_id-point.".$graph["point_id"].".png")) {
+					if(! array_key_exists($graph["group"], $ret)) {
+						$ret[$graph["group"]] = array();
+					}
+					$res = $db->execute($query, $graph["point_id"]);
+					$row = $res->fetchRow();
+					$description = $row["point_description"];
+					array_push($ret[$graph["group"]], array("name" => $graph["name"],
+										"src" => "graphs/$jobs_id/".$graph["name"],
+										"description" => $description));
 				}
-				$res = $db->execute($query, $graph["point_id"]);
-				$row = $res->fetchRow();
-				$description = $row["point_description"];
-				array_push($ret[$graph["group"]], array("name" => $graph["name"],
-									"src" => "graphs/$jobs_id/".$graph["name"],
-									"description" => $description));
 			}
+			print(json_encode(array("version" => 1, "graphs" => $ret)));
+		} else {
+			$query = $db->prepare("select point_description as description, name as group, pd.units as units from point_descriptions pd, point_groups pg where pd.point_groups_id = pg.id and point_name = ?");
+			$ret = array();
+			$pointsDir = sys_get_temp_dir()."/flot";
+			if(!file_exists($pointsDir)) {
+				mkdir($pointsDir);
+			}
+			$pointsDir .= "/$jobs_id";
+			if(! file_exists($pointsDir)) {
+				$phar = new PharData($pointsArchive);
+				$phar->extractTo($pointsDir);
+			}
+			$metadata = json_decode(file_get_contents($pointsDir."/metadata"));
+			foreach($metadata->points as $point) {
+				$res = $db->execute($query, $point);
+				$row = $res->fetchRow();
+				if(! array_key_exists($row["group"], $ret)) {
+					$ret[$row["group"]] = array();
+				}
+				array_push($ret[$row["group"]], array(	"name" => $point,
+									"src" => "graphs/$jobs_id/$point",
+									"unit" => $row["units"],
+									"description" => $row["description"]));
+			}
+			print(json_encode(array("version" => 2, "graphs" => $ret, "hosts" => $metadata->hosts)));
 		}
-		print(json_encode(array("version" => 1, "graphs" => $ret)));
 	}
 ?>
