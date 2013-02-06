ee7c4492f5e30e597783dde4de16fab0b479894e
Evan J. Felix <karcaw@gmail.com>
2013-02-06 12:58:40 -0800

    Look for nodejs as well in minify scripts

--- web/enyo/tools/minify.sh
+++ web/enyo/tools/minify.sh
@@ -8,10 +8,14 @@ ENYO="$TOOLS/.."
 MINIFY="$TOOLS/minifier/minify.js"
 
 # check for node, but quietly
+NODE=
 if command -v node >/dev/null 2>&1; then
-	# use node to invoke minify with a known path to enyo and imported parameters
-	node "$MINIFY" -enyo "$ENYO" $@
+	NODE=node
+elif command -v nodejs >/dev/null 2>&1; then
+	NODE=nodejs
 else
 	echo "No node found in path"
 	exit 1
 fi
+# use node to invoke minify with a known path to enyo and imported parameters
+$NODE "$MINIFY" -enyo "$ENYO" $@
