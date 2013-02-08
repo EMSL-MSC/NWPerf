2282db5e950471b468c4043743d643cc25cf3a49
Evan J. Felix <karcaw@gmail.com>
2013-02-08 11:01:22 -0800

    add in ceph tools

---
+++ lib/nwperf/nwperfceph.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+import rados
+import sys
+import os
+import numpy
+import time
+import cgi
+import struct
+import subprocess
+
+DAY=86400
+DAYMIN=DAY/60
+
+def prevmin(seconds):
+	return int(seconds/60.0)*60
+
+
+class RadosDataStore:
+	def __init__(self,cephconfig,cluster):
+		self.cluster = cluster
+		self.r_cluster = rados.Rados(conffile=cephconfig)
+		self.r_cluster.connect()
+		self.ioctx = self.r_cluster.open_ioctx(cluster+".points")
+		#host order should not change
+		self.hosts = self.ioctx.read("hostorder",65535).split("\n")[:-1]
+
+	def hostindex(self,o):
+		#silly hack to deal with pic and adding .local to everything
+		try:
+			return self.hosts.index(o)
+		except:
+			#return self.hosts.index(o+".local")
+			print "missing host:"+o
+			return 0 ## crappy hack for now.. deal with missing hosts somehow?
+
+	def getIndex(self):
+		data = self.ioctx.read('pointindex')
+		return data
+
+	def getXTicks(self,time=time.time()):
+		return self.hosts[:self.getHostCount(time)]
+
+	def getDescription(self,key):
+		data = self.ioctx.read("pointdesc",1024*1024)
+		lines=data.split("\n")[:-1]
+		for line in lines:
+			k,desc = line.split(":",1)
+			if k == key:
+				return desc
+		return "No Description in Ceph Database" 
+
+	def getRate(self,reftime,key):
+		daystr=time.strftime("%Y-%m-%d",time.gmtime(reftime-84600))
+		try:
+			unit = self.ioctx.get_xattr(daystr+"/"+key,"unit")
+		except rados.ObjectNotFound,msg:
+			unit="Error Getting It:"+daystr
+		return unit
+		
+	def getDataSlice(self,start,end,key,hostlist=None):
+		"""return a dataslice from the cluster"""
+		#print "getDS:",start,end,key
+		start = prevmin(start)
+		end   = prevmin(end)
+		hostcount=max(self.getHostCount(start),self.getHostCount(end))
+		#print "hostcount",hostcount
+		daysize=hostcount*1440
+	 
+		sday = DAY * (start / DAY) 
+		eday = DAY * (end / DAY)
+
+		days = range(sday,eday+1440,DAY)
+		daystr = [time.strftime("%Y-%m-%d",time.gmtime(i)) for i in days]
+
+		data = numpy.empty([hostcount,(end-start)/60],dtype='f4')
+		#print "Data",data.shape
+		
+		pi=0
+		si=start/60
+		#print "SI",si
+		for day,str in zip(days,daystr):
+			#print day
+			daym=day/60
+			dhostcount = self.getHostCount(day)
+			ei=min(end/60,daym+DAYMIN)
+			#print "EI",ei
+			#print "EI-SI",(ei-si),si-daym
+			if (ei-si):
+				try:
+					daydata = self.ioctx.read(str+"/"+key,length=(ei-si)*dhostcount*4,offset=(si-daym)*dhostcount*4)
+					#print "daydata",len(daydata),str
+					dayarray = numpy.frombuffer(daydata,dtype='f4')
+					dayarray.resize(ei-si,dhostcount)
+					dayarray=dayarray.transpose()
+				except rados.ObjectNotFound:
+					#if the file is not there yet, which happens around the day start, return blank data
+					#print "no file"
+					dayarray=numpy.zeros((hostcount,ei-si),dtype='f4')
+				except ValueError:
+					print "ValueError",daydata,ei,si
+					dayarray=numpy.zeros((hostcount,ei-si),dtype='f4')
+				#print dayarray.shape,daym
+				#print si,ei,pi,ei-si,pi+ei-si,si-daym,ei-daym
+
+				#print data[:dhostcount,pi:pi+ei-si].shape, dayarray.shape
+				#print dayarray.shape[1], pi+ei-si
+				#if dayarray[::].shape[1] == pi+ei-si:
+				#	dayarray=dayarray[:,:-1]
+				data[:dhostcount,pi:pi+ei-si]=dayarray[::]#[:,si-daym:ei-daym]
+
+			pi+=ei-si
+			si=ei
+		
+		if hostlist:
+			idx = [self.hostindex(o) for o in hostlist]
+			#throw out any indexes that are bigger than hostcount
+			idx = [i for i in idx if i<hostcount]
+			#print data.shape,idx
+			data = data.take(idx,0)
+
+		return data.take(range(data.shape[1]-1,-1,-1),1)
+
+	def getYTicks(self,start,end):
+		s = prevmin(start)/60
+		e = prevmin(end)/60
+		#print "S,E",s,e,e-s
+		ticks=[time.strftime("%F %R",time.localtime(i*60)) for i in xrange(s,e)]
+		ticks.reverse()
+		return ticks
+
+	def getHostCount(self,t):
+		#print "getHostCount("+str(t)+")"
+		num=0
+		hosl=self.ioctx.read("hostorder.sizelog")
+		hostsizes=hosl.split("\n")
+		for line in hostsizes:
+			if len(line)<1 or line[0]=='#':
+				continue
+			date,count=line.split()
+			d=int(date)
+			c=int(count)
+			if d<=t:
+				num=c
+		return num
+			
+		
+if __name__ == "__main__":
+
+	#rds = RadosDataStore("/etc/ceph/ceph.conf","chinook")
+	rds = RadosDataStore("/etc/ceph/ceph.conf","pic")
+	t=int(time.time())
+	d=rds.getDataSlice(t-(128*60),t,"cputotals.user")
+	d.tofile("/tmp/file")
+
+	print rds.getXTicks()
+	print rds.getYTicks(t-(128*60),t)
+	print rds.getHostCount(t)
+	print rds.getHostCount(1343718000)
+	print rds.getHostCount(1356940800)
+	
+
---
+++ sbin/nwperf-ceph-store.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+
+import zmq
+import time
+import sys
+#sys.path.append("/home/efelix/cephbuild/lib/python2.6/site-packages/")
+import rados
+import struct
+import simplejson as json
+import nwperf
+from nwperf import nwperfceph
+from operator import itemgetter
+
+
+seenobjects={}
+hostsizes={}
+
+def processWork(r_ioctx,worklist):
+	global seenobjects
+	write_count=0
+	print "Work",time.time(),sum([len(i) for i in worklist.values()])
+
+	for object in worklist.keys():
+		cur_offset=-0xEF
+		start_offset=0
+		data=''
+		thelist=worklist[object]
+		#print "Work",time.time(),len(thelist),object
+		thelist.sort(key=itemgetter(0))
+
+		if not seenobjects.has_key(object):
+			daysize=1440*hostsizes[object.split("/")[0]]
+			r_ioctx.trunc(object,daysize*4)
+			seenobjects[object]=1
+
+		for item in thelist:
+			offset,val = item
+			#print item
+
+			#case: new value fits after current stream
+			if cur_offset+4 == offset: 
+				data += struct.pack("<f",val)
+				cur_offset = offset
+
+			#case: new value is somewhere else in stream
+			else:
+				#flush old data
+				#print "write1", object,start_offset,len(data)
+				r_ioctx.aio_write(object,data,start_offset)
+				write_count+=1
+
+				#setup for new one
+				start_offset = offset
+				cur_offset=offset
+				data = struct.pack("<f",val)
+
+		#flush final line
+		#print "write2", cur_object,start_offset,len(data)
+		r_ioctx.aio_write(object,data,start_offset)
+		write_count+=1
+
+	print "Done",time.time(),sum([len(i) for i in worklist.values()]),write_count
+
+def connectZMQ(nsurl,points):
+	#zmq setup
+	z_ctx = zmq.Context()
+
+	z_sock_ns = z_ctx.socket(zmq.REQ)
+	z_sock_ns.connect(nsurl)
+
+	urls=[]
+	for name in points:
+		z_sock_ns.send_multipart(("1","getService",name))
+		urls+=z_sock_ns.recv_multipart()
+	print urls
+
+	z_sock_data = z_ctx.socket(zmq.SUB)
+	z_sock_data.setsockopt(zmq.SUBSCRIBE, "")
+	z_sock_data.setsockopt(zmq.RCVBUF, 4194304)
+	for url in urls[::2]:
+		if url != 'ERROR':
+			z_sock_data.connect(url)
+
+	z_poll = zmq.core.Poller()
+	z_poll.register(z_sock_data,zmq.POLLIN)
+
+	return z_sock_data,z_poll
+
+def main(zmqns,cephconfig,cluster):
+	objectinfo={}
+
+	#rados setup
+	r_cluster = rados.Rados(conffile=cephconfig)
+	r_cluster.connect()
+	r_ioctx = r_cluster.open_ioctx(cluster+".points")
+	rds=nwperfceph.RadosDataStore(cephconfig,cluster)
+
+	data = r_ioctx.read("hostorder",65535)
+	#hosts will have all hosts, even if the timestamp in hostorder.sizelog shows smaller.
+	hosts=data.split("\n")[:-1]
+	#hostcount=len(hosts)
+	#daysize=hostcount*1440
+	#print hostcount,daysize
+	id=0
+	hostorder={}
+	for host in hosts:
+		hostorder[host]=id
+		id+=1
+	try:
+		data = r_ioctx.read("pointindex",65535)
+		pointlist = data.split("\n")[:-1]
+	except rados.ObjectNotFound:
+		pointlist=[]
+
+
+	z_sock_data,z_poll = connectZMQ(zmqns,(cluster+".allpoints",))
+
+	lastdata=0;
+
+	try:
+		work={}
+		olddata=False
+		while True:
+			res = z_poll.poll(1000) #returns # of events ready
+			if res:
+				old=time.time()-3600
+				header,data = z_sock_data.recv_multipart()
+				if header == 'Point':
+					try:
+						data = json.loads(data)
+						#print data
+						# sample: {'host': 'cu01n101', 'time': '1343863450', 'val': '52', 'unit': 'percent', 'pointname': 'cputotals.user'}
+						secs = int(data['time'])
+						if secs < old:
+							olddata=data
+							continue
+						val = float(data['val'])
+						daystring = time.strftime("%Y-%m-%d",time.gmtime(secs))
+						object = daystring+"/"+data['pointname']
+						try:
+							location=hostorder[data['host'].replace(".local","")] 
+						except KeyError:
+							continue
+						try:
+							hostcount=hostsizes[daystring]
+						except KeyError:
+							hostcount=rds.getHostCount(secs)
+							print "hostcount",secs,hostcount
+							hostsizes[daystring]=hostcount
+						minute = secs%86400/60
+						offset = 4*(minute*hostcount+location)
+						#print object, minute, location, offset
+						if not data['pointname'] in pointlist:
+							print "adding",data['pointname']
+							r_ioctx.aio_append('pointindex',data['pointname']+"\n")
+							pointlist.append(data['pointname'])
+						try:
+							work[object].append((offset,val))
+						except KeyError:
+							work[object]=[(offset,val)]
+							r_ioctx.set_xattr(object, 'unit', data['unit'])
+					except ValueError,msg:
+						print "Error consuming a data point:",msg
+						print data
+				else:
+					#print header
+					pass
+			else:
+				if sum([len(i) for i in work.values()]):
+					#dump old keys
+					for key in work.keys():
+						if not work[key]:
+							del work[key]
+					processWork(r_ioctx,work)	
+					lastdata = time.time()
+
+				for key in work.keys():
+						work[key]=[]
+				#print len(work.keys())
+				if time.time()-lastdata > 120:
+					print "Reconnecting to zmq"
+					z_sock_data,z_poll = connectZMQ(zmqns,(cluster+".allpoints",))
+					#wait at least 30 seconds for next try
+					lastdata = time.time()-90
+				if olddata:
+					print "Old Data Seen, sample: ",olddata
+					olddata=False
+					
+	except KeyboardInterrupt:
+		pass
+	except zmq.ZMQError,msg:
+		print "ZMQ error, exiting:",msg
+			
+	r_ioctx.close()
+	r_cluster.shutdown()
+
+if __name__ == "__main__":
+	parser = nwperf.defaultServerOptionParser()
+	parser.add_option("-S","--name-server",dest="nameserver",type="string",help="The ZMQ URL of the nameserver to register with",default="tcp://nwperf-ns:6967")
+	parser.add_option("-c","--cluster",dest="cluster",type="string",help="The cluster prefix to publish as",default=None)
+	parser.add_option("-f","--ceph-config",dest="cephconfig",type="string",help="Location of the ceph config file",default="/etc/ceph/ceph.conf")
+
+	options,args = nwperf.parseServerOptions()
+
+	if not options.cluster:
+		parser.error("No Cluster Specified")
+
+	main(options.nameserver,options.cephconfig,options.cluster) 
+
