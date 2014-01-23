0340b8df5a4628272464e6cd00f0a8a9c9026977
Ken Schmidt <kenneth.schmidt@pnnl.gov>
2014-01-22 17:19:53 -0800

    Change curly quotes to regular quotes

--- sbin/nwperf-ceph-store.py
+++ sbin/nwperf-ceph-store.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Copyright 2013 Battelle Memorial Institute.
-# This software is licensed under the Battelle “BSD-style” open source license;
+# This software is licensed under the Battelle "BSD-style" open source license;
 # the full text of that license is available in the COPYING file in the root of the repository
 
 import zmq
